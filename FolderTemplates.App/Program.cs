fc69b47ba91b45db6177a5c950636e5e5ddbf134
Harvey <kracker.karakas@gmail.com>
2025-08-08 21:46:10 +0700

    Add Create Template form

--- FolderTemplates.App/Program.cs
+++ FolderTemplates.App/Program.cs
@@ -1,5 +1,6 @@
 namespace FolderTemplates.App
 {
+    using FolderTemplates.CommandLine;
     internal static class Program
     {
         /// <summary>
@@ -11,7 +12,31 @@ namespace FolderTemplates.App
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmCreateFolder(args));
+
+            if(args == null || args.Length == 0 )
+            {
+                return;
+            }
+
+            ConsoleCommandLine cmd = new();
+            cmd.RegisterParameter(new CommandLineParameter("sourceFolder", true, "The path of the Template Folder to process"));
+            cmd.Parse(args ?? Array.Empty<string>(), true, "sourceFolder");
+
+            if (cmd.ParsedSuccessfully)
+            {
+                string folderPath = (cmd["sourceFolder"].Exists ? cmd["sourceFolder"].Value : "") ?? "";
+                string ftFolderPath = Path.Combine(folderPath, ".ft");
+                string templatePath = Path.Combine(ftFolderPath, "template.json");
+
+                if (!Directory.Exists(ftFolderPath) || !File.Exists(templatePath))
+                {
+                    // Template doesn't exist, launch template creation form
+                    Application.Run(new frmCreateTemplate(args ?? Array.Empty<string>()));
+                    return;
+                }
+            }
+
+            Application.Run(new frmCreateFolder(args ?? Array.Empty<string>()));
         }
     }
 }
